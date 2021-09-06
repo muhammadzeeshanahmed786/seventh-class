a4dc0904f2d7dec53d350733b08b3b6384412f9e
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-06 11:57:55 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,217 @@
+
+
+
+
+
+
+// var cities=["Karachi" , "Lahore" , "Islamabad"]; 
+
+
+// cities.push("Peshawar");
+
+// cities.push("Quetta");
+
+// console.log(cities);
+
+
+
+// cities.unshift("Sakkar")
+
+// cities.unshift("Masehra")
+
+
+// console.log(cities)
+
+
+
+
+
+
+
+
+// add in first
+
+// var name1=["Zeeshan","Ahmed"];
+
+// name1.unshift("adnan")
+// console.log(name1)
+
+
+
+
+
+
+// add in last
+
+
+// var name2=["Adnan","Zeeshan"]
+
+// name2.push("Ahmed");
+// console.log(name2)
+
+
+
+
+
+
+
+
+
+// Delete from First
+
+
+// var names=["Zeeshan","Ahmed","Adnan"]
+
+// names.pop();
+
+// console.log(names)
+
+
+
+
+// Delete from last
+
+
+// var names=["Zeeshan","Ahmed","Adnan"]
+// names.shift();
+
+// console.log(names)
+
+
+
+
+
+
+// var names=["Zeeshan","Ahmed","Adnan","Hasnian","Ali","Israr","Ibrar","Abdullah"]
+
+
+// var a=names.indexOf("Ali")
+// names.splice(4,1)
+
+
+// // names.slice(2,3)
+// console.log(names)
+
+
+
+
+
+
+
+
+// var fruits=["Apple","Orange","Banana","Apple","Date"]
+
+// var a=fruits.lastIndexOf("Apple")
+// console.log(a)
+
+
+// var fruits=["Apple","Orange","Banana","Apple","Date"]
+// fruits.splice(2,1,"Mango")
+// console.log(fruits)
+
+
+
+// var pro1=prompt("Enter movie name")
+// var pro2=prompt("Enter movie name")
+// var pro3=prompt("Enter movie name")
+// var pro4=prompt("Enter movie name")
+
+
+// var movie=[];
+
+
+
+// movie.push(pro1);
+// movie.push(pro2);
+// movie.push(pro3);
+// movie.push(pro3);
+
+// var a= movie.length;
+
+// console.log(movie)
+
+// console.log("Length of array",a)
+
+
+
+
+
+
+
+// var user1=prompt("Enter Color in start  name")
+// var user2=prompt("Enter color in End")
+// var colorName=["Red","Green","White","Blue"]
+
+
+// // user.push(colorName)
+// colorName.unshift(user1)
+// colorName.push(user2)
+// console.log(colorName)
+
+
+
+
+
+// Ask user how many colors are added
+
+
+// var userIndex=+prompt("Enter  a number");
+
+// var pro=prompt("Enter Color Name")
+
+
+// var colorName=["Red","Green","White","Blue"]
+
+
+// colorName.splice(userIndex,0,pro)
+
+// console.log(colorName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Ask user how many colors are deleted this array 
+
+
+
+// var userIndex=+prompt("Enter  a number");
+
+// var pro=+prompt("How many Colors are Deleted ")
+
+
+// var colorName=["Red","Green","White","Blue"]
+
+
+// colorName.splice(userIndex,pro)
+
+// console.log(colorName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
